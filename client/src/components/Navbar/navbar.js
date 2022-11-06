e8c309770a21179f01cd0ec28fbd141c9f73be34
giuseppelazzara98 <s302064@studenti.polito.it>
2022-11-06 18:15:03 +0100

    Navbar implementation

---
+++ client/src/components/Navbar/navbar.js
@@ -0,0 +1,21 @@
+import { Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import {CiMountain1} from 'react-icons/ci';
+import Navbar from 'react-bootstrap/Navbar';
+import {  useNavigate } from 'react-router-dom';
+
+
+function NavbarHead(props) {
+  const navigate = useNavigate();
+  return (
+    <Navbar bg="light" variant="light">
+    <Container fluid>
+        
+    <Navbar.Brand > <CiMountain1 size={30} > </CiMountain1>HTracker</Navbar.Brand>
+    <Button variant="secondary" onClick={()=> navigate("/login")}> Login</Button>
+    </Container>
+  </Navbar>
+  );
+}
+
+export default NavbarHead;
