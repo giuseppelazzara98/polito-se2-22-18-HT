d16ade884b7fb6fafb72205c9a8925bfff5ed70c
Elisa <elisalel-1998@hotmail.com>
2022-11-06 12:23:08 +0100

    setting up routes app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './styles/base.scss';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<App2 />
+		</Router>
+	);
+}
+
+function App2() {
+	
+	return (
+		<div className="App">
+			<main className="main-wrap">
+					<Routes>
+            {/* to insert route */}
+					</Routes>
+			</main>
+		</div>
+	);
 }
 
 export default App;
