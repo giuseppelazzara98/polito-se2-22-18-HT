1cccf368423d1e324cd6ce4ed0c667d58c1cbe60
FRANCESCO ROSATI s296247 <s296247@studenti.polito.it>
2022-11-09 14:02:12 +0100

    Added filter queries

--- server/modules/hike-dao.js
+++ server/modules/hike-dao.js
@@ -18,8 +18,8 @@ class HikeDAO {
     // create the service table
     newHikeTable = () => {
         return new Promise((resolve, reject) => {
-            
-            const sql = "CREATE TABLE IF NOT EXISTS HIKE(id_hike INTEGER NOT NULL, id_start_place INTEGER NOT NULL, id_end_place INTEGER NOT NULL, description TEXT, length REAL, expected_time REAL, ascent INTEGER, difficulty TEXT, FOREIGN KEY(id_end_place) REFERENCES PLACE(id_place), FOREIGN KEY(id_start_place) REFERENCES PLACE(id_place), PRIMARY KEY(id_hike AUTOINCREMENT));";
+
+            const sql = "CREATE TABLE IF NOT EXISTS HIKE(id_hike INTEGER NOT NULL, name TEXT, id_start_place INTEGER NOT NULL, id_end_place INTEGER NOT NULL, description TEXT, length REAL, expected_time REAL, ascent INTEGER, difficulty INTEGER, geographical_area TEXT, FOREIGN KEY(id_end_place) REFERENCES PLACE(id_place), FOREIGN KEY(id_start_place) REFERENCES PLACE(id_place), PRIMARY KEY(id_hike AUTOINCREMENT));";
             this.db.run(sql, (err) => {
                 if (err) {
                     console.log('Error running sql: ' + sql);
@@ -47,27 +47,84 @@ class HikeDAO {
         });
     }
 
-    // get all hikes
-    getAllHikes = () => {
+    /* 
+        Difficulties -> Integer:
+
+        0 -> tourist
+        1 -> hiker
+        2 -> professional hiker
+
+    */
+
+    // get all filtered hikes
+    getAllFilteredHikes = (filters) => {
+
+        let sql = this.knex.select('HIKE.name', 'HIKE.id_hike', 'HIKE.description', 'P1.description as START_PLACE', 'P2.description as END_PLACE', 'HIKE.length', 'HIKE.expected_time', 'HIKE.ascent', 'HIKE.difficulty', 'HIKE.geographical_area')
+            .from('HIKE')
+            .join('PLACE as P1', { 'P1.id_place': 'HIKE.id_start_place' })
+            .join('PLACE as P2', { 'P2.id_place': 'HIKE.id_end_place' });
+
+        if (filters.geo_area.length !== 0) {
+            sql = sql.whereIn('HIKE.geographical_area', filters.geo_area);
+        }
+        if (filters.difficulty.length !== 0) {
+            sql = sql.whereIn('HIKE.difficulty', filters.difficulty);
+        }
+        if (filters.exp_time !== null) {
+            sql = sql.whereBetween('HIKE.expected_time', [filters.exp_time.min, filters.exp_time.max]);
+        }
+        if (filters.length !== null) {
+            sql = sql.whereBetween('HIKE.length', [filters.length.min, filters.length.max]);
+        }
+        if (filters.ascent !== null) {
+            sql = sql.whereBetween('HIKE.ascent', [filters.ascent.min, filters.ascent.max]);
+        }
+
+        //We can decide to do the query with knex or with sqlite
+
+        /*return new Promise((resolve, reject) => {
+            sql.then((rows) => {
+                const hikes = rows.map((el) => {
+                    return {
+                        name: el.name,
+                        key: el.id_hike,
+                        description: el.description,
+                        start_place: el.START_PLACE,
+                        end_place: el.END_PLACE,
+                        length: el.length,
+                        expected_time: el.expected_time,
+                        ascent: el.ascent,
+                        difficulty: el.difficulty,
+                        geographical_area: el.geographical_area,
+                    }
+                });
+                resolve(hikes);
+            }).catch((err) => {
+                console.log(err);
+                reject(err);
+            });
+        });*/
+
         return new Promise((resolve, reject) => {
-            const sql = this.knex.select('HIKE.description','HIKE.id_hike','P1.description as START_PLACE','P2.description as END_PLACE', 'HIKE.length','HIKE.expected_time','HIKE.ascent','HIKE.difficulty').from('HIKE').join('PLACE as P1',{'P1.id_place':'HIKE.id_start_place'}).join('PLACE as P2',{'P2.id_place':'HIKE.id_end_place'});
             this.db.all(sql.toString(), [], (err, rows) => {
                 if (err) {
                     console.log('Error running sql: ' + sql);
                     console.log(err);
                     reject(err);
                 } else {
-                    
+
                     const hikes = rows.map((el) => {
                         return {
-                            name: el.description,
+                            name: el.name,
                             key: el.id_hike,
+                            description: el.description,
                             start_place: el.START_PLACE,
                             end_place: el.END_PLACE,
                             length: el.length,
                             expected_time: el.expected_time,
                             ascent: el.ascent,
-                            difficulty: el.difficulty
+                            difficulty: el.difficulty,
+                            geographical_area: el.geographical_area,
                         }
                     });
                     resolve(hikes);
@@ -75,7 +132,6 @@ class HikeDAO {
             });
         });
     };
-
 }
 
 module.exports = HikeDAO;
--- server/routers/hike_router.js
+++ server/routers/hike_router.js
@@ -1,22 +1,84 @@
 'use strict';
 
 const express = require('express');
+const { check, body, validationResult } = require('express-validator');
 const router = express.Router();
 const hikeDao = require('../modules/DbManager').hike_dao;
 
 /*** Hikes APIs ***/
 
 // GET /api/hikes
+//TODO: delete this api when the post /api/hikes is ready
 router.get('/hikes', async (req, res) => {
 
     try {
-        const result = await hikeDao.getAllHikes();
+        const result = await hikeDao.getAllFilteredHikes(0);
         return res.status(200).json(result);
     }
-    catch(err) {
+    catch (err) {
         return res.status(500).json({ error: "Internal Server Error" });
     }
 
 });
+
+// Examples of body:
+/*
+    {
+        "geo_area": [],
+        "difficulty": ["tourist"],
+        "exp_time": { "min": 5.2, "max": 7.0 },
+        "length": { "min": 0.0, "max": 15.7 },
+        "ascent": { "min": 500, "max": 2000 }
+    }
+    {
+        "geo_area": ["nord est","nord ovest"],
+        "difficulty": ["professional hiker"],
+        "exp_time": { "min": 5.6, "max": 9.0 },
+        "length": { "min": 0.0, "max": 15.7 },
+        "ascent": { "min": 500, "max": 2900 }
+    }
+*/
+
+//POST /api/hikes
+router.post('/hikes',
+    /*body('issueDate').isString(),
+        body('supplierId').isInt({ min: 1 }),
+        body('products').isArray(),	
+        body('products.*.SKUId').isInt({ min: 1 }),
+        body('products.*.description').isString(),
+        body('products.*.price').isFloat(),
+        body('products.*.qty').isInt({ min: 1 }),*/
+    async (req, res) => {
+
+        //const errors = validationResult(req);
+
+        /*if (!(dayjs(req.body.issueDate, ['YYYY/MM/DD', 'YYYY/MM/DD HH:mm'], true).isValid())) {
+            console.log("Invalid date");
+            return res.status(422).json({ error: "Unprocessable Entity" });
+        }*/
+
+        /*if (!errors.isEmpty()) {
+            console.log("Validation of request body failed!");
+            return res.status(422).json({ errors: errors.array() });
+        }*/
+
+        if (Object.keys(req.body).length === 0) {
+            console.log("Empty body!");
+            return res.status(422).json({ error: "Empty body request" });
+        }
+
+        if (Object.keys(req.body).length !== 5) {
+            console.log("Data not formatted properly!");
+            return res.status(422).json({ error: "Data not formatted properly" });
+        }
+
+        try {
+            const result = await hikeDao.getAllFilteredHikes(req.body);
+            return res.status(200).json(result);
+        }
+        catch (err) {
+            return res.status(503).json({ error: "Service Unavailable" });
+        }
+    });
 
 module.exports = router;
