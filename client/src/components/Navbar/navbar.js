84f1db3dc9604a0d9805d2aaee7043138f5c16f0
Elisa <elisalel-1998@hotmail.com>
2022-11-13 20:24:14 +0100

    fix css navbar

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -2,14 +2,20 @@ import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import { CiMountain1 } from 'react-icons/ci';
 import { Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 import { useNavigate, useLocation } from 'react-router-dom';
 import styles from './index.module.scss';
 import API from '../../API/api';
+import { useMediaQuery }  from "react-responsive";
+import { maxBreakpoints } from "../../helpers/configs";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faRightToBracket, faRightFromBracket, faHouse } from '@fortawesome/free-solid-svg-icons';
 
 function NavbarHead(props) {
 	const navigate = useNavigate();
 	const location = useLocation();
-	
+	const isMobile = useMediaQuery({ maxWidth: maxBreakpoints.tabletLandscape });
+
 	const handleLogOut = async () => {
 		await API.logOut();
 		props.setLoggedIn(false);
@@ -25,26 +31,32 @@ function NavbarHead(props) {
 					<CiMountain1 color="white" size={30} />{' '}
 					<span className="text-white">HTracker </span>
 				</Navbar.Brand>
-				
-				
-				{props.loggedIn && props.user.role === 'guide' && (
-					<Button className={styles.button} onClick={() => navigate('/newHike')}>
-					New Hike
-				</Button>
-				)}
-				{!props.loggedIn && (
-					<Button className={styles.button} onClick={() => navigate('/login')}>
-						Login
-					</Button>
-				)}
-				{props.loggedIn && (
-					<>
-					<Button className={styles.button} onClick = {() => navigate('/')}>Home</Button>
-					<Button className={styles.button} onClick={handleLogOut}>
-						Logout
-					</Button>
-					</>
-				)}
+				<div className={styles.buttonsContainer}>
+					{props.loggedIn && props.user.role === 'guide' && location.pathname !== "/newHike" && (
+						<NavLink className={`btn ${styles.buttonNavLink}`} to="/newHike">
+							{!isMobile && "New Hike"}
+							{isMobile && <FontAwesomeIcon icon={faPlus}/>}
+						</NavLink>
+					)}
+					{!props.loggedIn && (
+						<Button className={styles.button} onClick={() => navigate('/login')}>
+							{!isMobile && "Login"}
+							{isMobile && <FontAwesomeIcon icon={faRightToBracket}/>}
+						</Button>
+					)}
+					{props.loggedIn && (
+						<>
+							{location.pathname !== "/" && <NavLink className={`btn ${styles.buttonNavLink}`} to="/">
+								{!isMobile && "Home"}
+								{isMobile && <FontAwesomeIcon icon={faHouse}/>}
+							</NavLink>}
+							<Button className={styles.button} onClick={handleLogOut}>
+								{!isMobile && "Logout"}
+								{isMobile && <FontAwesomeIcon icon={faRightFromBracket}/>}
+							</Button>
+						</>
+					)}
+				</div>
 			</Container>
 		</Navbar>
 	);
