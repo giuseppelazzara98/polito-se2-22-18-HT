6aa21a19f0340c718ae348273f81923d363a67ae
Ahmed Khater <s294633@studenti.polito.it>
2022-12-08 23:17:21 +0100

    Fix some sonarCloud minor issues

--- client/src/components/SelectFilter/SelectFilter.js
+++ client/src/components/SelectFilter/SelectFilter.js
@@ -1,36 +1,42 @@
-import React, { useState } from "react";
-import styles from "./index.module.scss";
+import styles from './index.module.scss';
 import Select from 'react-select';
 
-
 export default function SelectFilter(props) {
-  const {
-    title = "",
-    facets = [],
-    name = "",
-    filters = [],
-    isDisabled = false,
-    removeAndAddFilter = () => { },
-    inputValue = () => {},
-    setInputValue = () => {}
-  } = props;
+	const {
+		title = '',
+		facets = [],
+		name = '',
+		filters = [],
+		isDisabled = false,
+		removeAndAddFilter = () => {},
+		inputValue = () => {},
+		setInputValue = () => {}
+	} = props;
 
-  return (
-    <>
-      <h6 className={styles.filterTitle}>{title}</h6>
-      <Select
-        className={styles.customSelect}
-        classNamePrefix="select"
-        isClearable={true}
-        isSearchable={true}
-        isDisabled={isDisabled}
-        name={name}
-        options={facets}
-        onChange={(val) => removeAndAddFilter(name, val?.id)}
-        inputValue={inputValue}
-        onInputChange={(newString) => setInputValue(newString)}
-        value={facets?.filter(facet => facet.id === filters?.filter(filterSelected => filterSelected.key === name)?.[0]?.id)?.[0] || null}
-      />
-    </>
-  )
+	return (
+		<>
+			<h6 className={styles.filterTitle}>{title}</h6>
+			<Select
+				className={styles.customSelect}
+				classNamePrefix="select"
+				isClearable={true}
+				isSearchable={true}
+				isDisabled={isDisabled}
+				name={name}
+				options={facets}
+				onChange={(val) => removeAndAddFilter(name, val?.id)}
+				inputValue={inputValue}
+				onInputChange={(newString) => setInputValue(newString)}
+				value={
+					facets?.filter(
+						(facet) =>
+							facet.id ===
+							filters?.filter(
+								(filterSelected) => filterSelected.key === name
+							)?.[0]?.id
+					)?.[0] || null
+				}
+			/>
+		</>
+	);
 }
