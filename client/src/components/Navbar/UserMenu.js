0bd91f7f25924fdd367de34f8547fef05b0156d2
Elisa <elisalel-1998@hotmail.com>
2022-12-10 19:22:15 +0100

    fix menu

--- client/src/components/Navbar/UserMenu.js
+++ client/src/components/Navbar/UserMenu.js
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "./index.module.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRightFromBracket, faUser, faSquarePlus,faBed,faParking } from '@fortawesome/free-solid-svg-icons';
-import { useMediaQuery }  from "react-responsive";
+import { faRightFromBracket, faUser, faSquarePlus, faBed, faParking } from '@fortawesome/free-solid-svg-icons';
+import { useMediaQuery } from "react-responsive";
 import { maxBreakpoints } from "../../helpers/configs";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export default function UserMenu(props) {
   const {
@@ -12,32 +12,42 @@ export default function UserMenu(props) {
     handleLogOut,
   } = props;
   const isMobile = useMediaQuery({ maxWidth: maxBreakpoints.tabletLandscape });
-  const navigate=useNavigate();
+  const navigate = useNavigate();
+  const location = useLocation();
 
   return (
     <div className={styles.userContainer}>
       <div className={`btn ${styles.userInfoContainer}`}>
         {!isMobile && <span className={styles.userName}>{user.name} {user.surname}</span>}
-        <FontAwesomeIcon icon={faUser}/>
+        <FontAwesomeIcon icon={faUser} />
       </div>
       <div className={styles.optionModal}>
         {isMobile && <span className={styles.userName}>{user.name} {user.surname}</span>}
-        <div className={styles.option} onClick={() => navigate("/newHike")}>
-          <span className={styles.label}>New Hike </span>
-               <FontAwesomeIcon icon={faSquarePlus} className={styles.iconOption}/>
-          
-        </div>
-        <div className={styles.option} onClick={() => navigate("/newHut")}>
-          <span className={styles.label}>New Hut</span>
-            <FontAwesomeIcon icon={faBed} className={styles.iconOption}/>  
-        </div>
-        <div className={styles.option} onClick={() => navigate("/newParkingLot")}>
-          <span className={styles.label}>New Parking Lot</span>
-          <FontAwesomeIcon icon={faParking} className={styles.iconOption}/>
-        </div>
+        {user.role === 'Local guide' && (
+          <>
+            {location.pathname !== "/newHike" && (
+              <div className={styles.option} onClick={() => navigate("/newHike")}>
+                <span className={styles.label}>New Hike </span>
+                <FontAwesomeIcon icon={faSquarePlus} className={styles.iconOption} />
+              </div>
+            )}
+            {location.pathname !== "/newHut" && (
+              <div className={styles.option} onClick={() => navigate("/newHut")}>
+                <span className={styles.label}>New Hut</span>
+                <FontAwesomeIcon icon={faBed} className={styles.iconOption} />
+              </div>
+            )}
+            {location.pathname !== "/newParkingLot" && (
+              <div className={styles.option} onClick={() => navigate("/newParkingLot")}>
+                <span className={styles.label}>New Parking Lot</span>
+                <FontAwesomeIcon icon={faParking} className={styles.iconOption} />
+              </div>
+            )}
+          </>
+        )}
         <div className={styles.option} onClick={() => handleLogOut()}>
           <span className={styles.label}>Logout</span>
-          <FontAwesomeIcon icon={faRightFromBracket} className={styles.iconOption}/>
+          <FontAwesomeIcon icon={faRightFromBracket} className={styles.iconOption} />
         </div>
       </div>
     </div>
