8bdcba1fb6f6d59ae9dca57b0d6d8207cabec2b6
giuseppelazzara98 <s302064@studenti.polito.it>
2022-12-10 18:40:12 +0100

    Map radius fixing

--- client/src/components/RadiusMapFilter/Pointer.js
+++ client/src/components/RadiusMapFilter/Pointer.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useMapEvents, Circle } from 'react-leaflet';
 
 export default function Pointer(props)
 {
     const { radiusCenter, setRadiusCenter, modifyRangeFilter } = props;
+
     const map = useMapEvents({
         click: (ev) => {
             const latLng = map.mouseEventToLatLng(ev.originalEvent);
+            map.setView([ev.latlng.lat, ev.latlng.lng]);
             setRadiusCenter({ ...radiusCenter, center: [latLng.lat, latLng.lng] });
-            map.setView([latLng.lat, latLng.lng]);
-            modifyRangeFilter(radiusCenter);
-            
         }
 
 
     });
+    useEffect(() => {
+        modifyRangeFilter(radiusCenter);
+    }, [radiusCenter]);
+    
     return (
         <Circle
                 center = {
